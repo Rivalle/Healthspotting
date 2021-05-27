987bbe81490fd7904b0135f062ef46139913327f
Rivalle <alexderm55@gmail.com>
2021-05-27 19:48:22 +0300

    made a signup page

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>HealthSpotting</title>
   <link rel="stylesheet" type="text/css" href="styles.css">
-  <link rel="stylesheet" type="text/css" href="loginStyles.css">
+  <link rel="stylesheet" type="text/css" href="signupStyles.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -48,21 +48,19 @@
       </div>
     </nav>
   </div>
-  <!-- Username and Password -->
+  <!-- Register forms -->
   <div class="separator">
-    <h4>Log in to your account</h4>
+    <h4>Create an account</h4>
     <hr>
     <input type="text" class="form-control" placeholder="Username" autofocus="" autocomplete="off" maxlength="80" >
     <hr>
+    <input type="text" class="form-control" placeholder="Email Address" maxlength="25" >
+    <hr>
     <input type="password" class="form-control" placeholder="Password" maxlength="25" >
-      <div class="form-check">
-        <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
-        <label class="form-check-label" for="flexCheckChecked">
-          Remember me
-        </label>
-      </div>
+    <hr>
+    <input type="password" class="form-control" placeholder="Repeat Password" maxlength="25" >
     <button class="button1" type="button" name="button">Enter</button>
-    <button class="button2" type="button" name="button" onclick="window.location.href='signup.php'">Sign-up</button>
+    <button class="button2" type="button" name="button" onclick="window.location.href='login.php'">Log-in</button>
   </div>
   <!-- footer -->
   <footer class="footer-dark">
