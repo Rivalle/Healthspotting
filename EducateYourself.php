7b32c2801a5300784abffb5c53058cfb0b4a7b7c
Rivalle <alexderm55@gmail.com>
2021-05-29 14:24:32 +0300

    changed styles.css name to indexStyles.css

--- EducateYourself.php
+++ EducateYourself.php
@@ -3,7 +3,7 @@
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HealthSpotting</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="indexStyles.css">
     <link rel="stylesheet" type="text/css" href="Educatestyles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
